e6fad44324afff5834a92fe971733d89d2cc6db6
Takashi Masuda <masutaka.net@gmail.com>
2016-07-16 19:11:30 +0900

    Add routing `GET /organizations`

---
+++ spec/routing/organizations_routing_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe OrganizationsController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/organizations').to route_to('organizations#index')
+    end
+  end
+end
