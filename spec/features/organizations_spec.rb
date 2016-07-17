7bb856e2dcc61e665c1dcf6744a8d9fe497260e7
Takashi Masuda <masutaka.net@gmail.com>
2016-07-17 22:56:20 +0900

    Make feature spec simple

--- spec/features/organizations_spec.rb
+++ spec/features/organizations_spec.rb
@@ -3,14 +3,13 @@ feature 'Organizations' do
 
   given(:endpoint) { 'https://api.github.com/user/orgs' }
 
-  context 'given a user who belongs to `emacs-jp` and `feedforce`' do
+  context 'given a user who belongs to `feedforce`' do
     background do
-      stub_request_get(endpoint, fixture('user_orgs_emacs-jp-and-feedforce.json'))
+      stub_request_get(endpoint, fixture('user_orgs_feedforce.json'))
     end
 
     scenario 'shows the organizations' do
       visit '/organizations'
-      expect(page).to have_content 'emacs-jp'
       expect(page).to have_content 'feedforce'
     end
   end
@@ -22,7 +21,6 @@ feature 'Organizations' do
 
     scenario 'doesnot show any organizations' do
       visit '/organizations'
-      expect(page).to_not have_content 'emacs-jp'
       expect(page).to_not have_content 'feedforce'
     end
   end
