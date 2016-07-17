23416c9f48c919602180a64d314305b15dea902a
Takashi Masuda <masutaka.net@gmail.com>
2016-07-17 20:46:12 +0900

    Add feature /organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,4 +1,5 @@
 class OrganizationsController < ApplicationController
   def index
+    @organizations = Organization.all
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rspec'
 require 'webmock/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
