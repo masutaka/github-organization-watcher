73bf331dc9f1b98250c5c38a6999d2cbc5ce2115
Takashi Masuda <masutaka.net@gmail.com>
2016-07-18 15:50:28 +0900

    Add Organization.repositories_subscription

--- spec/support/webmock_helper.rb
+++ spec/support/webmock_helper.rb
@@ -5,4 +5,11 @@ module WebmockHelper
       body: response_body
     )
   end
+
+  def stub_request_get_404(uri, response_body)
+    WebMock.stub_request(:get, uri).to_return(
+      status: 404, headers: { 'Content-Type' => 'application/json' },
+      body: response_body
+    )
+  end
 end
