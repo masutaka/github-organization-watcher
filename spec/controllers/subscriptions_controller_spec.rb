4ddd2a8ca3056f2f4a8a26e0cd74c8ee88bafa80
Takashi Masuda <masutaka.net@gmail.com>
2016-07-31 22:19:03 +0900

    Fix CI failures

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,5 +1,10 @@
 RSpec.describe SubscriptionsController, type: :controller do
   describe '#update' do
+    before do
+      stub_request_put_200('https://api.github.com/repos/emacs-jp/emacs-jp.github.com/subscription',
+                           fixture('subscription_watch.json'))
+    end
+
     it 'is `201 Created`' do
       get :update, params: { owner: 'emacs-jp', repo: 'emacs-jp.github.com' }
       expect(response.status).to eq 201
@@ -7,6 +12,11 @@ RSpec.describe SubscriptionsController, type: :controller do
   end
 
   describe '#destroy' do
+    before do
+      stub_request_delete_200('https://api.github.com/repos/emacs-jp/emacs-jp.github.com/subscription',
+                              fixture('subscription_unwatch.json'))
+    end
+
     it 'is `200 OK`' do
       delete :destroy, params: { owner: 'emacs-jp', repo: 'emacs-jp.github.com' }
       expect(response.status).to eq 200
--- spec/support/webmock_helper.rb
+++ spec/support/webmock_helper.rb
@@ -14,4 +14,20 @@ module WebmockHelper
       body: response_body
     )
   end
+
+  def stub_request_put_200(uri, response_body)
+    WebMock.stub_request(:put, uri).to_return(
+      status: 200,
+      headers: { 'Content-Type' => 'application/json; charset=utf-8' },
+      body: response_body
+    )
+  end
+
+  def stub_request_delete_200(uri, response_body)
+    WebMock.stub_request(:delete, uri).to_return(
+      status: 200,
+      headers: { 'Content-Type' => 'application/json; charset=utf-8' },
+      body: response_body
+    )
+  end
 end
