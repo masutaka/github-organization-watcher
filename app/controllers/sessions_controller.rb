fa8a4c12980e7cace4391e485d82d3099027eae2
Takashi Masuda <masutaka.net@gmail.com>
2016-08-21 20:10:19 +0900

    Create a user at login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   def callback
+    auth = request.env['omniauth.auth']
+    user = User.find_or_create_with_omniauth_by!(auth)
+    session[:user_id] = user.id
     redirect_to root_path
   end
 end
