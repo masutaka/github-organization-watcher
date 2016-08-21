c0786863696fc1612379ffcf577329c36cfdfee9
Takashi Masuda <masutaka.net@gmail.com>
2016-08-21 19:44:44 +0900

    Add /

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'top#index'
+
   resources :orgs, only: [:index], param: :name do
     resources :repos, only: [:index]
   end
