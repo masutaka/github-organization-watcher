872a3e936b6706ac2af50e9c78a5022169505b16
Takashi Masuda <masutaka.net@gmail.com>
2016-08-26 00:32:13 +0900

    Avoid root_path with session

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,5 @@
 class TopController < ApplicationController
   def index
+    redirect_to orgs_path if logged_in?
   end
 end
--- spec/features/top_spec.rb
+++ spec/features/top_spec.rb
@@ -3,11 +3,16 @@ feature 'Top' do
 
   context 'given a session' do
     given(:user) { FactoryGirl.create(:user_taro) }
-    background { login_as user }
+    given(:github_endpoint) { 'https://api.github.com/user/orgs' }
+
+    background do
+      login_as user
+      stub_request_get_200(github_endpoint, fixture('user_orgs_emacs-jp.json'))
+    end
 
     scenario 'shows "Logout"' do
       visit '/'
-      expect(page).to have_link 'Logout', href: '/logout'
+      expect(current_path).to eq '/orgs'
     end
   end
 
