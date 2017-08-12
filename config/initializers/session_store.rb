8108ac96aeda0ef72cc0411093bf2d9f79497321
Takashi Masuda <masutaka.net@gmail.com>
2017-08-12 14:39:45 +0900

    Use redis as session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_github-organization-watcher_session'
+Rails.application.config.session_store :redis_store,
+                                       servers: "#{ENV['REDIS_URL_BASE']}/0/session"
