67e6b860993a4b6c93ccbcd5cbc853f10a6aba20
Takashi Masuda <masutaka.net@gmail.com>
2017-08-12 14:57:10 +0900

    Same to heroku-redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :redis_store,
-                                       servers: "#{ENV['REDIS_URL_BASE']}/0/session"
+                                       servers: "#{ENV['REDIS_URL']}/0/session"
