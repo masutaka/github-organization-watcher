7ca9b48451c07a8f747c2cd6d905511d957d75d9
Yoko TAMADA <tamada@feedforce.jp>
2018-06-14 14:20:09 +0900

    :sparkles: Modify organizations and repositories view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,9 +18,11 @@
 document.addEventListener('turbolinks:load', function(){
   $('.repo .conditions input:radio').on('change', function(e){
     var owner = location.pathname.match(/orgs\/(.*?)\//)[1];
-    var repo = $(this).parent().parent().parent().attr('id');
+    var repo = $(this).parent().parent().parent().parent();
+    var repo_id = repo.attr('id');
+    var prev_condition = repo.attr('class').match(/unwatching|watching|ignoring/i)[0];
     var next_condition = $(this).attr('class');
-    var path = '/repos/' + owner + '/' + repo + '/subscription';
+    var path = '/repos/' + owner + '/' + repo_id + '/subscription';
     var method = null, data = {};
 
     switch(next_condition) {
@@ -43,6 +45,8 @@ document.addEventListener('turbolinks:load', function(){
       $.ajax({
         method: method, url: path, data: data
       }).done(function(publishers, textStatus, jqXHR) {
+        repo.removeClass(prev_condition);
+        repo.addClass(next_condition);
         return true;
       }).fail(function(jqXHR, textStatus, errorThrown) {
         return false;
