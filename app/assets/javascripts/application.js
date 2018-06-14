4a24f8b1458b63a63ee206abd0a101a733085721
Yoko TAMADA <tamada@feedforce.jp>
2018-06-14 14:27:53 +0900

    :+1: Refactor jQuery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,12 @@
 
 document.addEventListener('turbolinks:load', function(){
   $('.repo .conditions input:radio').on('change', function(e){
+    var input_radio = $(this)
     var owner = location.pathname.match(/orgs\/(.*?)\//)[1];
-    var repo = $(this).parent().parent().parent().parent();
+    var repo = input_radio.parents('.repo');
     var repo_id = repo.attr('id');
     var prev_condition = repo.attr('class').match(/unwatching|watching|ignoring/i)[0];
-    var next_condition = $(this).attr('class');
+    var next_condition = input_radio.attr('class');
     var path = '/repos/' + owner + '/' + repo_id + '/subscription';
     var method = null, data = {};
 
