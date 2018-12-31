df52f751677f24862e24a91f2942e161116f3b9e
Takashi Masuda <masutaka.net@gmail.com>
2018-12-31 18:41:18 +0900

    Fix thw following warning
    DEPRECATION WARNING: Static attributes will be removed in FactoryBot 5.0. Please use dynamic
    attributes instead by wrapping the attribute value in a block:
    
    uid { "123456" }

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,8 +2,8 @@
 
 FactoryBot.define do
   factory :user_taro, class: User do
-    uid '123456'
-    nickname 'taro'
-    oauth_token 'd5c8c42eb783cc2304698c289dfcbe86c5b3e5d5'
+    uid { '123456' }
+    nickname { 'taro' }
+    oauth_token { 'd5c8c42eb783cc2304698c289dfcbe86c5b3e5d5' }
   end
 end
